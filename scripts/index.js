b483f3619f875f27b33baa3af1189059c3268ab6
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-05-11 16:33:53 +0100

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,32 @@
+const speGrad = 1.974714803;
+const speInt = 221.2076729;
+const zoomGrad = 1.974714803;
+const zoomInt = 221.2076729;
+const hbGrad = 3.949065339;
+const hbInt = 100.2986412;
+
+
 function calculateTestsPerBottle(testsPerSession) {
 
   if (testsPerSession > 500) {
     return null;
   }
 
-  const grad = 1.974714803;
-  const yInt = 221.2076729;
+  let grad;
+  let yInt;
+
+  const method = $("#method-selection option:selected").val();
+
+  if (method === "spe") {
+    grad = speGrad;
+    yInt = speInt;
+  } else if (method === "zoom") {
+    grad = zoomGrad;
+    yInt = zoomInt;
+  } else if (method === "hb") {
+    grad = hbGrad;
+    yInt = hbInt;
+  }
 
   let tests = (testsPerSession * grad) + yInt;
 
@@ -13,8 +34,9 @@ function calculateTestsPerBottle(testsPerSession) {
     return 500;
   }
 
-  return Math.round(tests);
+  return tests;
 };
+
 
 function calculateBottles(testsPerSession, daysPerWeek, weeksPerYear) {
 
